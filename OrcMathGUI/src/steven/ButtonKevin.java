7e649f8f1509f291b72ddfc7cb6305de342757ec
TrishulaFaeris <31857240+TrishulaFaeris@users.noreply.github.com>
2018-01-08 11:49:02 -0500

    Update ButtonKevin.java

--- OrcMathGUI/src/steven/ButtonKevin.java
+++ OrcMathGUI/src/steven/ButtonKevin.java
@@ -10,6 +10,7 @@ import guiTeacher.components.Button;
 public class ButtonKevin extends Button implements ButtonInterfaceSteven{
 
 	public Color color;
+	public boolean chosen;
 	public ButtonKevin(int x, int y, int w, int h, String text, Color color, Action action) {
 		super(x, y, w, h, text, color, action);
 		// TODO Auto-generated constructor stub
@@ -17,6 +18,7 @@ public class ButtonKevin extends Button implements ButtonInterfaceSteven{
 
 	public ButtonKevin(int x, int y, int w, int h, String text, Action action) {
 		super(x, y, w, h, "", null);
+		chosen = false;	
 		// TODO Auto-generated constructor stub
 	}
 
@@ -40,7 +42,7 @@ public class ButtonKevin extends Button implements ButtonInterfaceSteven{
 	public void drawButton(Graphics2D g, boolean hover) {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		g.
+		g.setColor
 	}
 
 }
